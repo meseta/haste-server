d7b508a9e657647e578d3cc8ffcfcbe7cd294854
Yuan <meseta@gmail.com>
2018-04-15 02:03:25 +0100

    changed db

--- config/default.js
+++ config/default.js
@@ -23,11 +23,7 @@ module.exports = {
   },
 
   storage: {
-    type: "redis",
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379,
-    db: process.env.REDIS_DB || 2,
-    expire: process.env.REDIS_EXPIRE || 2592000
+    type: "postgres",
   },
 
   documents: {
--- config/docker.js
+++ config/docker.js
@@ -23,11 +23,8 @@ module.exports = {
   },
 
   storage: {
-    type: "redis",
-    host: process.env.REDIS_PORT_6379_TCP_ADDR || process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT_6379_TCP_PORT || process.env.REDIS_PORT || 6379,
-    db: process.env.REDIS_DB || 2,
-    expire: process.env.REDIS_EXPIRE || 2592000
+    type: "postgres",
+
   },
 
   documents: {
